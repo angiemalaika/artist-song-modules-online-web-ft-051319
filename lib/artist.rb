db21643ec4d806d38a9de2a61ee71684c6b8502a
Angela Mumba <malaika.mumba@gmail.com>
2019-06-17 03:22:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,7 +14,11 @@ class Artist
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
-
+  
+ def self.reset_all
+    self.all.clear
+  end
+  
   def self.all
     @@artists
   end
